ee776fe5afb1e2d2b41ec6625c976fee1a87200e
Valemm <33071776+Valemm@users.noreply.github.com>
2019-09-01 06:07:52 -0400

    Installing dependencies

--- nodejs_debian.sh
+++ nodejs_debian.sh
@@ -1,3 +1,7 @@
 sudo apt-get install curl software-properties-common -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt-get install gcc g++ make
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 sudo apt-get install nodejs -y
